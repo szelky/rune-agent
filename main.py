404250275bd83a3219215bf03015d74f925ac2a5
szelky <yigitkose7@gmail.com>
2025-06-25 01:20:42 +0300

    added --verbose command

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def main():
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+    verbose = False
+    if args[-1] == "--verbose":
+        verbose = True
+        args.pop()
     # Join the arguments to form the user prompt as a single string
     user_prompt = " ".join(args)
     # Initialize the Google GenAI client
@@ -26,19 +30,22 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, verbose)
 
 # Function to generate content using the Google GenAI client
 # It takes the client and a list of messages as input
 # and prints the response text
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
     print("Response:")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    if verbose:
+        print(f"User prompt: {" ".join(sys.argv[1:])}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":

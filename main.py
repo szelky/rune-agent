fdec90ec383533f4a7bd43a79fe23b8e58b2a01f
szelky <yigitkose7@gmail.com>
2025-06-25 00:47:25 +0300

    added main and generate_content function

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
-import os, sys
-from dotenv import load_dotenv
+import sys
+import os
 from google import genai
+from google.genai import types
+from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-# Initialize the Gemini client
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
 
-# Check if command line argument is provided
-if len(sys.argv) != 2:
-    print('Usage: python3 main.py "Input text"')
-    sys.exit(1) # Exit if no input text is provided
-    
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    args = sys.argv[1:]
+    # Check if the user provided a prompt
+    # If not, print usage instructions and exit with 1
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    # Join the arguments to form the user prompt as a single string
+    user_prompt = " ".join(args)
+    # Initialize the Google GenAI client
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+# Function to generate content using the Google GenAI client
+# It takes the client and a list of messages as input
+# and prints the response text
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    print("Response:")
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+
+
+if __name__ == "__main__":
+    main()
